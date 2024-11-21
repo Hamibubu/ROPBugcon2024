aab6797867366b1b90cafa0c99667a6ec89385c5
Abraham <108554878+Hamibubu@users.noreply.github.com>
2024-11-20 20:30:28 -0600

    Update rop1.c

--- rop1.c
+++ rop1.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-void rop1(){
-    printf("ROP 1!");
+int variable=0;
+
+void salto1() 
+{
+    printf("Lograste llegar al primer salto\n");
+    variable++;
 }
 
-int vulnerable(char *str){
-    char buff[300];
-    printf("[+] Copiando tu input!")
+void salto2() {
+    printf("Lograste el salto 2!\n");
+    variable++;
+}
+
+void salto3() {
+    printf("Llegaste al salto 3, vamos a comprobar si lo hiciste bien...\n");
+    if(variable==2){
+        system("/bin/bash");
+    }else{
+    	printf("Al parecer no lo estas haciedo bien...");
+    }
+}
+
+void vulnerable(char* string) 
+{
+    char buffer[100];
     strcpy(buffer, string);
 }
 
-int main(int argc, char** argv){
-    printf("Bienvenido al sistema de copiado de datos...\n\n")
+int main(int argc, char** argv) 
+{
+    vulnerable(argv[1]);
+    return 0;
 }
